264db355a2a6595ad4750ce9fad9249839f6eb8d
Александр Иконников <ikonnikovAI@lad24.ru>
2022-03-12 12:17:57 +0300

    add splash screen

--- App.tsx
+++ App.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Navigator from './src/navigation/Navigator';
 import {ThemeProvider} from 'styled-components';
 import {myTheme} from './src/theme/theme';
 import {Provider} from 'react-redux';
 import store from './src/store/store';
 import {injectStore} from './src/services/HttpService';
+import SplashScreen from 'react-native-splash-screen';
 
 injectStore(store);
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <Provider store={store}>
       <ThemeProvider theme={myTheme}>
--- android/app/src/main/java/com/balance/MainActivity.java
+++ android/app/src/main/java/com/balance/MainActivity.java
@@ -1,7 +1,7 @@
 package com.balance;
 
 import com.facebook.react.ReactActivity;
-
+import org.devio.rn.splashscreen.SplashScreen;
 import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
@@ -17,6 +17,7 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this);
     super.onCreate(null);
   }
 }
